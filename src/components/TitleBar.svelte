dcdc7a932653fdc0e2e99d64c4bb46d69f48bf84
54ac <me@54ac.ovh>
2024-05-07 21:24:25 +0200

    add icon to title bar

--- src/components/TitleBar.svelte
+++ src/components/TitleBar.svelte
@@ -1,3 +1,9 @@
 <div class="title-bar">
-	<div class="title-bar-text">Setup</div>
+	<div class="title-bar-text"><span id="title-bar-icon">📝</span> Setup</div>
 </div>
+
+<style>
+	#title-bar-icon {
+		line-height: 100%;
+	}
+</style>
